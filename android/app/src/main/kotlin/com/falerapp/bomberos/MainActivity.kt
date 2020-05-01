4525f2bacaf15fd23ac6aade0863b9db0086ad18
SergioEric <sericmorales@gmail.com>
2020-05-01 09:08:16 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/falerapp/bomberos/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.falerapp.bomberos
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
